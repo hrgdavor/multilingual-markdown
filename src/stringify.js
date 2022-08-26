4735a7e13a7fadf753e5f9906efe0f24576b3fdb
Davor Hrg <hrgdavor@gmail.com>
2022-08-26 13:05:59 +0200

    stringify hide option

--- src/stringify.js
+++ src/stringify.js
@@ -1,4 +1,4 @@
-export default function stringify(mdObj, fileName = 'md', skipInfo) {
+export default function stringify(mdObj, fileName = 'md', skipInfo, skipHidden) {
   return toLines(mdObj, [], skipInfo).join('\n')
 }
 
@@ -6,31 +6,36 @@ function stringifyInfo(info) {
   return `(${JSON.stringify(info)})`
 }
 
-function toLines(obj, out = [], skipInfo) {
+function toLines(obj, out = [], skipInfo, skipHidden) {
+  const pushInfo = () => {
+    if (!skipInfo && obj.info) out.push(stringifyInfo(obj.info))
+  }
   if (obj instanceof Array) {
     obj.forEach(line => {
       if (typeof line === 'string') {
         out.push(line)
       } else {
-        toLines(line, out)
+        toLines(line, out, skipHidden)
       }
     })
-  } else if (obj.sections) {
-    if (!skipInfo && obj.info) out.push(stringifyInfo(obj.info))
-    obj.sections.forEach(section => toLines(section, out))
-  } else if (obj.title !== undefined) {
-    let title = obj.title
-    if (obj.level) out.push(title)
-    if (!skipInfo && obj.info) out.push(stringifyInfo(obj.info))
-    toLines(obj.lines, out)
-  } else {
-    // code
-    let code = '```'
-    if (obj.code) code += obj.code
-    out.push(code)
-    if (!skipInfo && obj.info) out.push(stringifyInfo(obj.info))
-    toLines(obj.lines, out)
-    out.push('```')
+  } else if (!(skipHidden && obj.info?.hidden)) {
+    if (obj.sections) {
+      pushInfo()
+      obj.sections.forEach(section => toLines(section, out, skipHidden))
+    } else if (obj.title !== undefined) {
+      let title = obj.title
+      if (obj.level) out.push(title)
+      pushInfo()
+      toLines(obj.lines, out, skipHidden)
+    } else {
+      // code
+      let code = '```'
+      if (obj.code) code += obj.code
+      out.push(code)
+      pushInfo()
+      toLines(obj.lines, out, skipHidden)
+      out.push('```')
+    }
   }
 
   return out
